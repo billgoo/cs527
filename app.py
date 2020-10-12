216752c0fe3af4ce6aaa5bee086eedf97357694a
xw310LF <xinyang.wang@lotusflare.com>
2020-10-12 13:56:39 -0400

    add js and data base connection

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from mysql import buildQueryFromInput, connect_mysql
 
 app = Flask(__name__)
 
@@ -7,8 +9,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/mysql', methods = ['POST'])
+def queryMysql():
+    raw_query = request.form['query']
+    # return render_template('index.html', query=raw_query)
+    q = buildQueryFromInput(raw_query)
 
+    connection = connect_mysql(host='cs527project1group5.cnpt9dsbfddc.us-east-1.rds.amazonaws.com',
+                               user='admin',
+                               password='cs527project1',
+                               db='')
 
+    col_name, res, query_time = connection.run_query(q)
+    return render_template('index.html',
+                           col_name=col_name,
+                           res=res,
+                           query_time=query_time,
+                           query=raw_query)
 
 if __name__ == '__main__':
     app.run()
---
+++ mysql.py
@@ -0,0 +1,27 @@
+import pymysql
+from time import time
+import re
+
+def buildQueryFromInput(raw_query):
+    return re.sub('\W+', ' ', raw_query)
+
+class connect_mysql():
+    def __init__(self, host, user, password, db):
+        self.db = pymysql.connect(host, user, password, db)
+        self.cursor = self.db.cursor()
+
+    def make_query(self, query):
+        start_time = int(round(time() * 1000))
+        self.cursor.execute(query)
+        col_info = self.cursor.description
+        result = self.cursor.fetchall()
+        query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+        if len(result) > 100:
+            result = result[:99]
+        col_name = []
+        for i in range(len(col_info)):
+            col_name.append(col_info[i][0])
+        return col_name, result, query_time
+
+    def disconnect(self):
+        self.db.close()
